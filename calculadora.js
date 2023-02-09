4b9011afab1a4420bfa9e261756fa6a5b92c50b4
Raul S <raul.sr92@gmail.com>
2023-02-08 19:18:21 -0500

    Condicionales en la calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,25 +1,32 @@
-
 // capturar elementos html <input> en variables
-
 const precio = document.querySelector("#precio-etiqueta");
-console.log(precio.value);
+console.log(precio);
 
 const discount = document.querySelector("#descuento");
 
 // capturar elementos html <button> en variables (escuchador de evento)
-
 const botonCalcular = document.querySelector(".button-calcule");
 
 botonCalcular.addEventListener("click", calcularDescuento);
 
+// capturar elemento html para mostrar el descuento
 const resultadoFinal = document.querySelector(".resultado");
 
+// capturar elemento html para mostrar advertencia
+const resultadoFinalAdv = document.querySelector(".advertencia");
 
 function calcularDescuento(){
     const precioInteger = parseInt(precio.value);
     const discountInteger = parseInt(discount.value);
-    return resultadoFinal.innerHTML = (precioInteger*(100-discountInteger))/100;
-}
+    if (discountInteger>40){
+			return resultadoFinalAdv.innerHTML = "El descuento máximo es 40%."
+		} else if(!precioInteger ||!discountInteger){
+			return resultadoFinalAdv.innerHTML = "Ingrese el precio o descuento para efectuar el cálculo."
+		} else{
+			return resultadoFinal.innerHTML = 
+			"Su precio con dcto. es " + (precioInteger*(100-discountInteger))/100 + " $";
+		}
+};
 
 
 
